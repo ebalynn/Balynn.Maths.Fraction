35b06dbe9b2979f96d4c3a92d9682aeb6315dd15
Elliot Balynn <elliot@balynn.com>
2017-08-04 15:57:52 +0100

    Added examples project

---
+++ examples/Balynn.Maths.Examples.Fraction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balynn.Maths.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Fraction(2, 6);
+            var b = new Fraction(1, 3);
+
+            Console.WriteLine($"{a} == {b} is {a == b}");
+            Console.WriteLine($"{a} > {b} is {a > b}");
+            Console.WriteLine($"{a} > {5} is {a > 5L}");
+
+            var f = Fraction.FromDecimal(1.5m);
+            Console.WriteLine($"{1.5m} == {f}");
+            Console.WriteLine($"{f} * {a} = {f * a}");
+            Console.WriteLine($"Pi = {Fraction.Pi.ToDecimal()}");
+            Console.Read();
+        }
+    }
+}
