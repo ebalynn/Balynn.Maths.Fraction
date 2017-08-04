9fa98c0f0b5b11ba77b20c0a0fdaec81853e289e
Elliot Balynn <elliot@balynn.com>
2017-08-04 14:04:46 +0100

    Minor changes

--- tests/Balynn.Math.PerformanceTests.Fraction/Program.cs
+++ tests/Balynn.Math.PerformanceTests.Fraction/Program.cs
@@ -16,15 +16,15 @@ namespace Balynn.Math.PerformanceTests
                 while (true)
                 {
                     Thread.Sleep(TimeSpan.FromSeconds(1));
-                    Console.Write(".");
+                    Console.WriteLine(".");
                 }
             });
             var list = new List<Fraction>();
             var sw = new Stopwatch();
             sw.Start();
-            for (int i = 1; i < 66; i++)
+            for (int i = 1; i < 50; i++)
             {
-                for (int j = 1; j < 66; j++)
+                for (int j = 1; j < 50; j++)
                 {
                     list.Add(new Fraction(i, j));
                 }
@@ -42,10 +42,31 @@ namespace Balynn.Math.PerformanceTests
                     var r2 = f1 < f2;
                     var r3 = f1 >= f2;
                     var r4 = f1 <= f2;
+
+                    var r5 = f1 > 1L;
+                    var r6 = f1 < 1L;
+                    var r7 = f1 >= 1L;
+                    var r8 = f1 >= 1L;
+
+                    var r9 = f1 > 1f;
+                    var r10 = f1 < 1f;
+                    var r11 = f1 >= 1f;
+                    var r12 = f1 >= 1f;
+
+                    var r13 = f1 > 1d;
+                    var r14 = f1 < 1d;
+                    var r15 = f1 >= 1d;
+                    var r16 = f1 >= 1d;
+
+                    var r17 = f1 > 1m;
+                    var r18 = f1 < 1m;
+                    var r19 = f1 >= 1m;
+                    var r20 = f1 >= 1m;
+
                 }
             }
             sw.Stop();
-            Console.WriteLine($"Time taken to perform {list.Count * list.Count * 4} comparisons {sw.Elapsed}");
+            Console.WriteLine($"Time taken to perform comparisons on {list.Count * list.Count} items: {sw.Elapsed}");
 
             sw.Restart();
             for (int i = 0; i < list.Count; i++)
@@ -58,11 +79,18 @@ namespace Balynn.Math.PerformanceTests
                     var r2 = f1 - f2;
                     var r3 = f1 * f2;
                     var r4 = f1 / f2;
+
+                    f1 += 1L;
+                    f2 += 2L;
+                    var rf = f1 + 1f;
+                    var rd = f1 + 1d;
+                    var rm = f1 + 1m;
+
                 }
             }
             sw.Stop();
-            Console.WriteLine($"Time taken to perform {list.Count * list.Count * 4} ariphmetic operations {sw.Elapsed}");
-            Console.Read();
+            Console.WriteLine($"Time taken to perform ariphmetic operations on {list.Count * list.Count} items: {sw.Elapsed}");
+            //Console.Read();
         }
     }
 }
